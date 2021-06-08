76416bf95ef04efa79c27c6a32678cdf2e742e3e
eunyoungkim <snu9451@naver.com>
2021-06-08 16:15:09 +0900

    로그설정때문에 프로젝트 이동 spring4_1_1 to spring4_1_2

---
+++ spring4_1_2/src/web/mvc/Board41MDao.java
@@ -0,0 +1,24 @@
+package web.mvc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.vo.BoardMVO;
+
+public class Board41MDao {
+	Logger logger = Logger.getLogger(Board41MDao.class);
+	private SqlSessionTemplate sqlSessionTemplate = null;
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		this.sqlSessionTemplate = sqlSessionTemplate;
+	}
+	public List<Map<String, Object>> getBoardList(Map<String, Object> pmap) {
+		List<Map<String, Object>> boardList = null;
+		List<BoardMVO> boardList2 = null;
+		boardList = sqlSessionTemplate.selectList("getBoardList",pmap);
+		return boardList;
+	}
+	
+}
